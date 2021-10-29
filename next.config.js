481f7b27fb0fd049bbafb26f381302d3f78451e8
lucas homer <lucas.homer@gmail.com>
2021-10-29 15:25:45 -0700

    ok I landed on mdx-bundler

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const withMDX = require("@next/mdx")({
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: []
-  },
-  extension: /\.mdx$/
-});
-
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
