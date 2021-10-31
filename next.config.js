972b0089fff2ca91be47644bdd0bfb0e1c362469
lucas homer <lucas.homer@gmail.com>
2021-10-30 17:33:40 -0700

    fix self-hosted font loading issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,46 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/fonts/JosefinSans-VariableFont_wght.woff2",
+        headers: [
+          {
+            key: "Cache-control",
+            value: "public, immutable, max-age=31536000"
+          }
+        ]
+      },
+      // {
+      //   source: "/fonts/JosefinSans-Italic-VariableFont_wght.woff2",
+      //   headers: [
+      //     {
+      //       key: "Cache-control",
+      //       value: "public, immutable, max-age=31536000"
+      //     }
+      //   ]
+      // },
+      {
+        source: "/fonts/PlayfairDisplay-VariableFont_wght.woff2",
+        headers: [
+          {
+            key: "Cache-control",
+            value: "public, immutable, max-age=31536000"
+          }
+        ]
+      }
+      // {
+      //   source: "/fonts/PlayfairDisplay-Italic-VariableFont_wght.woff2",
+      //   headers: [
+      //     {
+      //       key: "Cache-control",
+      //       value: "public, immutable, max-age=31536000"
+      //     }
+      //   ]
+      // }
+    ];
+  },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
   experimental: {
