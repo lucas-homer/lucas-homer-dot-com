630bdb42e046fb997cb1fe04c58d9b681f890ff9
lucas homer <lucas.homer@gmail.com>
2021-11-02 21:42:31 -0700

    copy over some more basics

---
+++ components/Footer.tsx
@@ -0,0 +1,72 @@
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+// import NowPlaying from 'components/NowPlaying';
+
+const ExternalLink = ({
+  href,
+  children
+}: PropsWithChildren<{ href: string }>) => (
+  <a
+    className="text-gray-500 hover:text-gray-600 transition"
+    target="_blank"
+    rel="noopener noreferrer"
+    href={href}
+  >
+    {children}
+  </a>
+);
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full mb-8">
+      <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
+
+      {/* <NowPlaying /> */}
+      <div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-16 sm:grid-cols-3">
+        <div className="flex flex-col space-y-4">
+          <Link href="/">
+            <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
+          </Link>
+          <Link href="/about">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              About
+            </a>
+          </Link>
+          {/* <Link href="/newsletter">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Newsletter
+            </a>
+          </Link> */}
+        </div>
+        <div className="flex flex-col space-y-4">
+          <ExternalLink href="https://twitter.com/lucas_c_homer">
+            Twitter
+          </ExternalLink>
+          <ExternalLink href="https://github.com/lucas-homer">
+            GitHub
+          </ExternalLink>
+        </div>
+        <div className="flex flex-col space-y-4">
+          <Link href="/uses">
+            <a className="text-gray-500 hover:text-gray-600 transition">Uses</a>
+          </Link>
+          {/* <Link href="/guestbook">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Guestbook
+            </a>
+          </Link> */}
+          <Link href="/notes">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Notes
+            </a>
+          </Link>
+          {/* <Link href="/tweets">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Tweets
+            </a>
+          </Link> */}
+        </div>
+      </div>
+    </footer>
+  );
+}
