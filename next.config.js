6ec359f43e08c11250fc1cf43492870748083479
lucas homer <lucas.homer@gmail.com>
2021-11-11 20:37:49 -0800

    spitballing at this point

--- next.config.js
+++ next.config.js
@@ -1,67 +1,14 @@
-const { withContentlayer } = require("next-contentlayer");
+const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 
 module.exports = withContentlayer()({
-  // async headers() {
-  //   return [
-  // {
-  //   source: "/fonts/JosefinSans-VariableFont_wght.woff2",
-  //   headers: [
-  //     {
-  //       key: "Cache-control",
-  //       value: "public, immutable, max-age=31536000"
-  //     }
-  //   ]
-  // },
-  // {
-  //   source: "/fonts/JosefinSans-Italic-VariableFont_wght.woff2",
-  //   headers: [
-  //     {
-  //       key: "Cache-control",
-  //       value: "public, immutable, max-age=31536000"
-  //     }
-  //   ]
-  // },
-  // {
-  //   source: "/fonts/PlayfairDisplay-VariableFont_wght.woff2",
-  //   headers: [
-  //     {
-  //       key: "Cache-control",
-  //       value: "public, immutable, max-age=31536000"
-  //     }
-  //   ]
-  // }
-  // {
-  //   source: "/fonts/PlayfairDisplay-Italic-VariableFont_wght.woff2",
-  //   headers: [
-  //     {
-  //       key: "Cache-control",
-  //       value: "public, immutable, max-age=31536000"
-  //     }
-  //   ]
-  // }
-  //   ];
-  // },
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  swcMinify: true,
-  reactStrictMode: true,
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  // swcMinify: true,
   images: {
     domains: [
-      "i.scdn.co", // Spotify Album Art
-      "pbs.twimg.com" // Twitter Profile Picture
+      'i.scdn.co', // Spotify Album Art
+      'pbs.twimg.com' // Twitter Profile Picture
     ],
-    formats: ["image/avif", "image/webp"]
-  },
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat"
-      });
-    }
-
-    return config;
+    formats: ['image/avif', 'image/webp']
   }
 });
