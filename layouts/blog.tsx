e016b17468acefc2ef52d9177305b4f8bf976a3c
lucas homer <lucas.homer@gmail.com>
2021-11-26 17:59:22 -0800

    slight tweak to blog layout

--- layouts/blog.tsx
+++ layouts/blog.tsx
@@ -34,7 +34,7 @@ export default function BlogLayout({ children, topicsData, frontMatter }) {
           </h1>
           <div className="flex flex-col items-start justify-between w-full mt-2 md:flex-row md:items-center ">
             <div className="flex items-center">
-              <p className="ml-2 text-sm text-gray-700 dark:text-gray-300">
+              <p className="text-sm text-gray-700 dark:text-gray-300">
                 {'Lucas Homer / '}
                 {format(parseISO(frontMatter.publishedAt), 'MMMM dd, yyyy')}
               </p>
