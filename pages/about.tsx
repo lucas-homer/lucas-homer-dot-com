ee65fa621dd24d8565dc943f12aa4559b78f7ed2
lucas homer <lucas.homer@gmail.com>
2021-12-04 13:03:43 -0800

    fix typo in about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link'
+import Link from 'next/link';
 import type { InferGetStaticPropsType } from 'next';
 
 import Container from 'components/Container';
@@ -22,32 +22,53 @@ export default function Notes({
           my humble web home. Thanks for visiting. I hope you stay a while!
         </p>
         <p className="text-gray-600 dark:text-gray-300 prose prose-lg">
-          At my job at SelectQuote, where we follow <a href="https://engineering.atspotify.com/2014/03/27/spotify-engineering-culture-part-1/">
-          the Spotify model</a>, I started using our frontend chapter meetings (think
-          "brown bag" or "knowledge transfer") as a vehicle to learn by
-          teaching. It's been a boon for myself and others involved, but I want
-          to do more, and <a href="https://www.swyx.io/learn-in-public/">learn in public</a>. How about the open web, then?
+          At my job at SelectQuote, where we follow{' '}
+          <a href="https://engineering.atspotify.com/2014/03/27/spotify-engineering-culture-part-1/">
+            the Spotify model
+          </a>
+          , I started using our frontend chapter meetings (think "brown bag" or
+          "knowledge transfer") as a vehicle to learn by teaching. It's been a
+          boon for myself and others involved, but I want to do more, and{' '}
+          <a href="https://www.swyx.io/learn-in-public/">learn in public</a>.
+          How about the open web, then?
         </p>
         <p className="text-gray-600 dark:text-gray-300 prose prose-lg">
-          Things here fall into two buckets — <Link href="/blog"><a>blogs</a></Link> and <Link href="/notes"><a>notes</a></Link> — the former being
-          more fully formed thoughts, and the latter more loose references,
-          or maybe a <a href="https://maggieappleton.com/garden-history">digital garden</a>, if you will? Perhaps more of a, erm,
-          digital compost pile (dibs!). My hope is the notes pile up, break down (have I
-          lost you yet?), and over time turn into more fully formed blog posts.
+          Things here fall into two buckets —{' '}
+          <Link href="/blog">
+            <a>blogs</a>
+          </Link>{' '}
+          and{' '}
+          <Link href="/notes">
+            <a>notes</a>
+          </Link>{' '}
+          — the former being more fully formed thoughts, and the latter more
+          loose references, or maybe a{' '}
+          <a href="https://maggieappleton.com/garden-history">digital garden</a>
+          , if you will? Perhaps more of a, erm, digital compost pile (dibs!).
+          My hope is the notes pile up, break down (have I lost you yet?), and
+          over time turn into more fully formed blog posts.
         </p>
         <p className="text-gray-600 dark:text-gray-300 prose prose-lg">
-          Outside of extending tenuous metaphors, I also love to spend time outdoors, hiking and trail running. I'm also an avid cook, and love exploring the embarrassment of riches that is California's food and wine scene. A native Midwesterner, I'm originally from Kansas City, and hold undergraduate and law degrees from the University of Kansas.`}
+          Outside of extending tenuous metaphors, I also love to spend time
+          outdoors, hiking and trail running. I'm also an avid cook, and love
+          exploring the embarrassment of riches that is California's food and
+          wine scene. A native Midwesterner, I'm originally from Kansas City,
+          and hold undergraduate and law degrees from the University of Kansas.
         </p>
         <p className="text-gray-600 dark:text-gray-300 prose prose-lg">
-          After several years working in litigation, I switched gears to software,
-          completing the <a href="https://launchcode.org">Launchcode</a> LC101 and Apprenticeship programs. Whether
-          it's music, statutes, or code, it's all language and patterns to me. Among other reasons, I especially
+          After several years working in litigation, I switched gears to
+          software, completing the{' '}
+          <a href="https://launchcode.org">Launchcode</a> LC101 and
+          Apprenticeship programs. Whether it's music, statutes, or code, it's
+          all language and patterns to me. Among other reasons, I especially
           love software because I get that perfect mix of creativity and
           analysis.
         </p>
         <p className="text-gray-600 dark:text-gray-300 prose prose-lg">
-          Want to chat? Don't be a stranger. <a href="https://twitter.com/lucas-homer">Tweet at me</a> or <a href="mailto:lucas.homer@hey.com">send me an email</a>.
-          I'd love to hear from you.
+          Want to chat? Don't be a stranger.{' '}
+          <a href="https://twitter.com/lucas_c_homer">Tweet at me</a> or{' '}
+          <a href="mailto:lucas.homer@hey.com">send me an email</a>. I'd love to
+          hear from you.
         </p>
       </div>
     </Container>
